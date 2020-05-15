fd02f8549dca3daac9ea829f1a645efc31b16d7e
hophi123 <hoangphi120398@gmail.com>
2020-05-15 15:41:57 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CallApiService } from '../call-api.service';
+import { Routes, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private callApi: CallApiService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
+  login(){
+    const phonenumber = document.getElementById('phonenumber')['value'];
+    const password = document.getElementById('password')['value'];
+    if(phonenumber === '' || password === ''){
+      alert('Please enter all field');
+    }
 
+    const body = {
+      phone: phonenumber,
+      password: password
+    }
+
+    this.callApi.login(body).subscribe(res =>{
+      const code = res['meta'].code;
+      console.log(code);
+
+      if(code === '200'){
+        alert('Login Success');
+        this.router.navigate(['home'])
+        const token = res['data'].token;
+        const userInfo = JSON.stringify(res['data'].user);
+        localStorage.setItem("token", token);
+        localStorage.setItem("userInfo", userInfo);
+
+      }
+
+
+    }, err =>{
+      console.log(err);
+      const messageErr = err.error.meta.message;
+      alert(messageErr)
+
+    })
+
+  }
 }
